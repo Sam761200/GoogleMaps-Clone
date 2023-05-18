695e144a6867ecaa4c628aabd71c5c0c19571afd
Samuel761200 <samuel.saunier761200@gmail.com>
2023-05-18 16:25:17 +0200

    first commit

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+Api needed : API Javascript Maps (console.cloud.google.com)
+RapidAPI : Travel advisor (rapidapi.com)
+
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
 ## Getting Started
--- components/Map.jsx
+++ components/Map.jsx
@@ -10,7 +10,7 @@ const Map = ({ coordinates, setCoordinates, setBounds, places }) => {
   return (
     <Box width={"full"} height={"full"}>
       <GoogleMapReact
-        bootstrapURLKeys={{ key: "AIzaSyATEHrOezClUoUyOiGaFBlowEYWNlmDD1o" }}
+        bootstrapURLKeys={{ key: "YOUR_API_KEY" }}
         defaultCenter={coordinates}
         center={coordinates}
         defaultZoom={10}
